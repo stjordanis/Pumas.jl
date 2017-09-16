480659aa296dbe744ab2e53a6b20e81200405e5b
Vijay Ivaturi <ivijaydeep@gmail.com>
2017-09-16 13:23:44 -0400

    added 5 more event types

--- test/template_model_ev_system.jl
+++ test/template_model_ev_system.jl
@@ -56,15 +56,14 @@ sol = getsol(model=f,num_dv=2) # get both gut and central amounts and concentrat
 
 
 # ev2 - infusion into the central compartment - use ev2.csv in PKPDSimulator/examples/event_data/
-
-# new
 # amt=100: 100 mg infusion into central compartment
 
 # new
 # cmt=2: in the system of diffeq's, central compartment is the second compartment
 
 # new
-# rate=10: the dose is given at a rate of amt/time (mg/hr), i.e, 10mg/hr
+# rate=10: the dose is given at a rate of amt/time (mg/hr), i.e, 10mg/hr. In this example the 100mg amount
+# is given over a duration (DUR) of 10 hours
 
 # addl=3: 4 doses total, 1 dose at time zero + 3 additional doses (addl=3)
 # ii=12: each additional dose is given with a frequency of ii=12 hours
@@ -87,10 +86,12 @@ sol = getsol(model=f,num_dv=2) # get both gut and central amounts and concentrat
 sol = getsol(model=f,num_dv=1) # get central amounts  and concentrations amt/V
 
 
-# ev3 - infusion into the central compartment with lag time - use ev3.csv in PKPDSimulator/examples/event_data/
+# ev3 - infusion into the central compartment with lag time 
+# - use ev3.csv in PKPDSimulator/examples/event_data/
 # amt=100: 100 mg infusion into central compartment
 # cmt=2: in the system of diffeq's, central compartment is the second compartment
-# rate=10: the dose is given at a rate of amt/time (mg/hr), i.e, 10mg/hr
+# rate=10: the dose is given at a rate of amt/time (mg/hr), i.e, 10mg/hr. In this example the 100mg amount
+# is given over a duration (DUR) of 10 hours
 
 # new
 # LAGT=5: there is a lag of 5 hours after dose administration when amounts from the event
@@ -119,10 +120,12 @@ sol = getsol(model=f,num_dv=1) # get central amounts  and concentrations amt/V
 sol = getsol(model=f,num_dv=1) # get central amounts  and concentrations amt/V
 
 
-# ev4 - infusion into the central compartment with lag time - use ev4.csv in PKPDSimulator/examples/event_data/
+# ev4 - infusion into the central compartment with lag time and bioavailability 
+# - use ev4.csv in PKPDSimulator/examples/event_data/
 # amt=100: 100 mg infusion into central compartment
 # cmt=2: in the system of diffeq's, central compartment is the second compartment
-# rate=10: the dose is given at a rate of amt/time (mg/hr), i.e, 10mg/hr
+# rate=10: the dose is given at a rate of amt/time (mg/hr), i.e, 10mg/hr. In this example the 100mg amount
+# is given over a duration (DUR) of 10 hours
 # LAGT=5: there is a lag of 5 hours after dose administration when amounts from the event
 # are populated into the central compartment. Requires developing a new internal variable called 
 # ALAG_<comp name> or ALAG_<comp_num> that takes a value that delays the entry of dose into that compartment
@@ -147,8 +150,255 @@ sol = getsol(model=f,num_dv=1) # get central amounts  and concentrations amt/V
     0,    #MODE
     2,    #DUR2
     10,   #RAT2
-    0.412     #BIOAV
+    0.412 #BIOAV
 ]
 
 # corresponding mrgsolve and NONMEM solution in data4.csv in PKPDSimulator/examples/event_data/
+sol = getsol(model=f,num_dv=1) # get central amounts  and concentrations amt/V
+
+
+# ev5 - infusion into the central compartment at steady state (ss) 
+# - use ev5.csv in PKPDSimulator/examples/event_data/
+# amt=100: 100 mg infusion into central compartment
+# cmt=2: in the system of diffeq's, central compartment is the second compartment
+# rate=10: the dose is given at a rate of amt/time (mg/hr), i.e, 10mg/hr. In this example the 100mg amount
+# is given over a duration (DUR) of 10 hours
+# BIOAV=0.412: required developing a new internal variable called F_<comp name> or F_<comp num>,
+# where F is the fraction of amount that is delivered into the compartment. e.g. in this case, 
+# only 41.2 % of the 100 mg dose is administered at the 10mg/hr rate will enter the system.
+# F_<comp> is one of the most commonly estimated parameters in NLME
+
+#new
+#ss=1:  indicates that the dose is a steady state dose, and that the compartment amounts are to be reset 
+#to the steady-state amounts resulting from the given dose. Compartment amounts resulting from prior 
+#dose event records are "zeroed out," and infusions in progress or pending additional doses are cancelled
+
+# addl=3: 4 doses total, 1 dose at time zero + 3 additional doses (addl=3)
+# ii=12: each additional dose is given with a frequency of ii=12 hours
+# evid = 1: indicates a dosing event
+# mdv = 1: indicates that observations are not avaialable at this dosing record
+
+
+θ = [ 
+    1.5,  #Ka
+    1.0,  #CL
+    30.0, #V
+    0,    #LAGT
+    0,    #MODE
+    2,    #DUR2
+    10,   #RAT2
+    0.412,#BIOAV
+    1     #ss
+]
+
+# corresponding mrgsolve and NONMEM solution in data5.csv in PKPDSimulator/examples/event_data/
+sol = getsol(model=f,num_dv=1) # get central amounts  and concentrations amt/V
+
+# ev6 - infusion into the central compartment at steady state (ss), where frequency of events (ii) is less 
+# than the infusion duration (DUR)
+# - use ev6.csv in PKPDSimulator/examples/event_data/
+# amt=100: 100 mg infusion into central compartment
+# cmt=2: in the system of diffeq's, central compartment is the second compartment
+# rate=10: the dose is given at a rate of amt/time (mg/hr), i.e, 10mg/hr. In this example the 100mg amount
+# is given over a duration (DUR) of 10 hours
+
+#new
+# BIOAV=0.812: required developing a new internal variable called F_<comp name> or F_<comp num>,
+# where F is the fraction of amount that is delivered into the compartment. e.g. in this case, 
+# only 81.2 % of the 100 mg dose is administered at the 10mg/hr rate will enter the system.
+# F_<comp> is one of the most commonly estimated parameters in NLME
+
+#ss=1:  indicates that the dose is a steady state dose, and that the compartment amounts are to be reset 
+#to the steady-state amounts resulting from the given dose. Compartment amounts resulting from prior 
+#dose event records are "zeroed out," and infusions in progress or pending additional doses are cancelled
+# addl=3: 4 doses total, 1 dose at time zero + 3 additional doses (addl=3)
+
+#new
+# ii=6: each additional dose is given with a frequency of ii=6 hours
+
+# evid = 1: indicates a dosing event
+# mdv = 1: indicates that observations are not avaialable at this dosing record
+
+
+θ = [ 
+    1.5,  #Ka
+    1.0,  #CL
+    30.0, #V
+    0,    #LAGT
+    0,    #MODE
+    2,    #DUR2
+    10,   #RAT2
+    0.812,#BIOAV
+    1     #ss
+]
+
+# corresponding mrgsolve and NONMEM solution in data6.csv in PKPDSimulator/examples/event_data/
+sol = getsol(model=f,num_dv=1) # get central amounts  and concentrations amt/V
+
+
+# ev7 - infusion into the central compartment at steady state (ss), where frequency of events (ii) is less 
+# than the infusion duration (DUR)
+# - use ev7.csv in PKPDSimulator/examples/event_data/
+# amt=100: 100 mg infusion into central compartment
+# cmt=2: in the system of diffeq's, central compartment is the second compartment
+# rate=10: the dose is given at a rate of amt/time (mg/hr), i.e, 10mg/hr. In this example the 100mg amount
+# is given over a duration (DUR) of 10 hours
+
+#new
+# BIOAV=1: required developing a new internal variable called F_<comp name> or F_<comp num>,
+# where F is the fraction of amount that is delivered into the compartment. e.g. in this case, 
+# only 81.2 % of the 100 mg dose is administered at the 10mg/hr rate will enter the system.
+# F_<comp> is one of the most commonly estimated parameters in NLME
+
+#ss=1:  indicates that the dose is a steady state dose, and that the compartment amounts are to be reset 
+#to the steady-state amounts resulting from the given dose. Compartment amounts resulting from prior 
+#dose event records are "zeroed out," and infusions in progress or pending additional doses are cancelled
+# addl=3: 4 doses total, 1 dose at time zero + 3 additional doses (addl=3)
+
+#new
+# ii=6: each additional dose is given with a frequency of ii=6 hours
+
+# evid = 1: indicates a dosing event
+# mdv = 1: indicates that observations are not avaialable at this dosing record
+
+
+θ = [ 
+    1.5,  #Ka
+    1.0,  #CL
+    30.0, #V
+    0,    #LAGT
+    0,    #MODE
+    2,    #DUR2
+    10,   #RAT2
+    1,    #BIOAV
+    1     #ss
+]
+
+# corresponding mrgsolve and NONMEM solution in data7.csv in PKPDSimulator/examples/event_data/
+sol = getsol(model=f,num_dv=1) # get central amounts  and concentrations amt/V
+
+# ev8 - infusion into the central compartment at steady state (ss), where frequency of events (ii) is a 
+# multiple of infusion duration (DUR)
+# - use ev8.csv in PKPDSimulator/examples/event_data/
+# amt=100: 100 mg infusion into central compartment
+# cmt=2: in the system of diffeq's, central compartment is the second compartment
+
+#new
+# rate=8.33333: the dose is given at a rate of amt/time (mg/hr), i.e, 8.333333mg/hr. In this example the 100mg amount
+# is given over a duration (DUR) of 12 hours
+
+
+# BIOAV=1: required developing a new internal variable called F_<comp name> or F_<comp num>,
+# where F is the fraction of amount that is delivered into the compartment. e.g. in this case, 
+# only 81.2 % of the 100 mg dose is administered at the 10mg/hr rate will enter the system.
+# F_<comp> is one of the most commonly estimated parameters in NLME
+
+#ss=1:  indicates that the dose is a steady state dose, and that the compartment amounts are to be reset 
+#to the steady-state amounts resulting from the given dose. Compartment amounts resulting from prior 
+#dose event records are "zeroed out," and infusions in progress or pending additional doses are cancelled
+# addl=3: 4 doses total, 1 dose at time zero + 3 additional doses (addl=3)
+
+#new
+# ii=6: each additional dose is given with a frequency of ii=6 hours
+
+# evid = 1: indicates a dosing event
+# mdv = 1: indicates that observations are not avaialable at this dosing record
+
+
+θ = [ 
+    1.5,  #Ka
+    1.0,  #CL
+    30.0, #V
+    0,    #LAGT
+    0,    #MODE
+    2,    #DUR2
+    10,   #RAT2
+    1,    #BIOAV
+    1     #ss
+]
+
+# corresponding mrgsolve and NONMEM solution in data8.csv in PKPDSimulator/examples/event_data/
+sol = getsol(model=f,num_dv=1) # get central amounts  and concentrations amt/V
+
+
+# ev9 - infusion into the central compartment at steady state (ss), where frequency of events (ii) is  
+# exactly equal to infusion duration (DUR)
+# - use ev9.csv in PKPDSimulator/examples/event_data/
+# amt=100: 100 mg infusion into central compartment
+# cmt=2: in the system of diffeq's, central compartment is the second compartment
+
+#new
+# rate=10: the dose is given at a rate of amt/time (mg/hr), i.e, 10mg/hr. In this example the 100mg amount
+# is given over a duration (DUR) of 10 hours
+
+#new
+# BIOAV=0.412: required developing a new internal variable called F_<comp name> or F_<comp num>,
+# where F is the fraction of amount that is delivered into the compartment. e.g. in this case, 
+# only 81.2 % of the 100 mg dose is administered at the 10mg/hr rate will enter the system.
+# F_<comp> is one of the most commonly estimated parameters in NLME
+
+#ss=1:  indicates that the dose is a steady state dose, and that the compartment amounts are to be reset 
+#to the steady-state amounts resulting from the given dose. Compartment amounts resulting from prior 
+#dose event records are "zeroed out," and infusions in progress or pending additional doses are cancelled
+# addl=3: 4 doses total, 1 dose at time zero + 3 additional doses (addl=3)
+
+#new
+# ii=10: each additional dose is given with a frequency of ii=10 hours
+
+# evid = 1: indicates a dosing event
+# mdv = 1: indicates that observations are not avaialable at this dosing record
+
+
+θ = [ 
+    1.5,  #Ka
+    1.0,  #CL
+    30.0, #V
+    0,    #LAGT
+    0,    #MODE
+    2,    #DUR2
+    10,   #RAT2
+    0.412,#BIOAV
+    1     #ss
+]
+
+# corresponding mrgsolve and NONMEM solution in data9.csv in PKPDSimulator/examples/event_data/
+sol = getsol(model=f,num_dv=1) # get central amounts  and concentrations amt/V
+
+# ev10 - infusion into the central compartment at steady state (ss), where frequency of events (ii) is  
+# exactly equal to infusion duration (DUR)
+# - use ev10.csv in PKPDSimulator/examples/event_data/
+# amt=100: 100 mg infusion into central compartment
+# cmt=2: in the system of diffeq's, central compartment is the second compartment
+# rate=10: the dose is given at a rate of amt/time (mg/hr), i.e, 10mg/hr. In this example the 100mg amount
+# is given over a duration (DUR) of 10 hours
+
+#new
+# BIOAV=1: required developing a new internal variable called F_<comp name> or F_<comp num>,
+# where F is the fraction of amount that is delivered into the compartment. e.g. in this case, 
+# only 81.2 % of the 100 mg dose is administered at the 10mg/hr rate will enter the system.
+# F_<comp> is one of the most commonly estimated parameters in NLME
+
+#ss=1:  indicates that the dose is a steady state dose, and that the compartment amounts are to be reset 
+#to the steady-state amounts resulting from the given dose. Compartment amounts resulting from prior 
+#dose event records are "zeroed out," and infusions in progress or pending additional doses are cancelled
+# addl=3: 4 doses total, 1 dose at time zero + 3 additional doses (addl=3)
+# ii=10: each additional dose is given with a frequency of ii=10 hours
+
+# evid = 1: indicates a dosing event
+# mdv = 1: indicates that observations are not avaialable at this dosing record
+
+
+θ = [ 
+    1.5,  #Ka
+    1.0,  #CL
+    30.0, #V
+    0,    #LAGT
+    0,    #MODE
+    2,    #DUR2
+    10,   #RAT2
+    1,    #BIOAV
+    1     #ss
+]
+
+# corresponding mrgsolve and NONMEM solution in data10.csv in PKPDSimulator/examples/event_data/
 sol = getsol(model=f,num_dv=1) # get central amounts  and concentrations amt/V
