c1c40925fd6c55efd2b662887ba4f3440225ed81
Christopher Rackauckas <Contact@ChrisRackauckas.com>
2017-11-25 09:01:07 -0800

    reorganize

--- src/PKPDSimulator.jl
+++ src/PKPDSimulator.jl
@@ -6,13 +6,13 @@ using DiffEqBase, DiffEqMonteCarlo, Distributions, Reexport, DataFrames
 
 @reexport using OrdinaryDiffEq
 
-include("event_types.jl")
-include("data_read.jl")
-include("analytical_solution_type.jl")
+include("data_parsing/event_types.jl")
+include("data_parsing/data_read.jl")
+include("analytical_solutions/analytical_solution_type.jl")
+include("analytical_solutions/standard_models.jl")
 include("simulate_methods/utils.jl")
 include("simulate_methods/diffeqs.jl")
 include("simulate_methods/analytical.jl")
-include("standard_models.jl")
 
 export Person, Population, process_data
 
