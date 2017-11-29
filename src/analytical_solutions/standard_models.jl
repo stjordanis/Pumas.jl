e60641d1da20ec78a65de0023b636b599eeaaddb
Simon Byrne <simonbyrne@gmail.com>
2017-11-29 15:00:49 -0800

    simplify analytic expression

--- src/analytical_solutions/standard_models.jl
+++ src/analytical_solutions/standard_models.jl
@@ -10,12 +10,15 @@ end
 
 export ImmediateAbsorptionModel
 
-function one_compartment_f(t,t0,u0,dose,p,aux)
-  D = dose+aux
-  next_aux = exp(-(t-t0)*p.Ka)*D
-  next_u = exp(-t*(p.Ka+p.CL/p.V))*(D*exp(p.Ka*t0+p.CL*t/p.V)*p.Ka*p.V +
-  exp(t*p.Ka+p.CL*t0/p.V)*(u0*p.CL - (u0 + D)*p.Ka*p.V))/(p.CL-p.Ka*p.V)
-  next_u,next_aux
+function one_compartment_f(t,t0,C0,dose,p,D0)
+  Ka = p.Ka              # absorption rate
+  Ke = p.CL / p.V        # elimination rate
+  D0 += dose             # initial depot value
+  Sa = exp(-(t-t0)*Ka)
+  Se = exp(-(t-t0)*Ke)
+  D  = D0 * Sa           # next depot
+  C =  Ka / (Ka - Ke) * D0 * (Se - Sa) + C0 * Se # next central
+  C,D
 end
 
 function OneCompartmentModel(tf)
