75c270ff639ac92725853d8424ddd0ee489fac4a
Christopher Rackauckas <Contact@ChrisRackauckas.com>
2017-11-30 16:29:55 -0800

    fix output reduction

--- src/simulate_methods/diffeqs.jl
+++ src/simulate_methods/diffeqs.jl
@@ -1,5 +1,5 @@
 function simulate(_prob::ODEProblem,set_parameters,θ,ηi,datai::Person,
-                  output_reduction = (sol,p,datai) -> (sol,false),
+                  output_reduction = (sol,p,datai) -> sol,
                   alg = Tsit5();kwargs...)
   VarType = promote_type(eltype(ηi),eltype(θ))
   p = set_parameters(θ,ηi,datai.z)
