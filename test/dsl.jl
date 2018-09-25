e9f12500aad0d4f32ac29474e8bcc53e53f4de7d
Xingjian Guo <xg703@nyu.edu>
2018-09-25 14:05:42 -0400

    Fix test script to use same random seed

--- test/dsl.jl
+++ test/dsl.jl
@@ -108,10 +108,12 @@ sol2 = solve(mobj,subject,x0,y0)
 
 @test likelihood(mdsl,subject,x0,y0) ≈ likelihood(mobj,subject,x0,y0)
 
-@test simobs(mdsl,subject,x0,y0).derived.obs_cmax == simobs(mobj,subject,x0,y0).derived.obs_cmax
+Random.seed!(1); obs_dsl = simobs(mdsl,subject,x0,y0)
+Random.seed!(1); obs_obj = simobs(mobj,subject,x0,y0)
 
-@test (Random.seed!(1); simobs(mdsl,subject,x0,y0)[:dv]) ≈
-      (Random.seed!(1); simobs(mobj,subject,x0,y0)[:dv])
+@test obs_dsl.derived.obs_cmax == obs_obj.derived.obs_cmax
+
+@test map(x -> x.dv, obs_dsl) ≈ map(x -> x.dv, obs_obj)
 
 # Now test an array-based version
 
