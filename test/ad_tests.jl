13622ceda4f060cb88c419f5e57c37b4e68feb59
Xingjian Guo <xg703@nyu.edu>
2018-10-30 13:27:26 -0400

    Rewrite AD tests to include likelihood

--- test/ad_tests.jl
+++ test/ad_tests.jl
@@ -1,4 +1,5 @@
-using PuMaS, ForwardDiff, DiffEqDiffTools, Test
+using PuMaS, ForwardDiff, DiffEqDiffTools, Test, Random
+
 AD_gradient = ForwardDiff.gradient
 AD_hessian = ForwardDiff.hessian
 FD_gradient = DiffEqDiffTools.finite_difference_gradient
@@ -8,77 +9,116 @@ FD_hessian = function (f, x)
     FD_jacobian(grad_fun, x, Val{:central}, eltype(x), Val{false})
 end
 
-subject = process_nmtran(example_nmtran_data("event_data/data2"),
-                         [], [:cp])[1]
+data = process_nmtran(example_nmtran_data("data1"),
+                      [:sex,:wt,:etn])
+subject = data.subjects[1]
+# add a small epsilon to time 0 observations
+obs1 = subject.observations[1]
+if obs1.time == 0
+    subject.observations[1] = PuMaS.Observation(sqrt(eps()), obs1.val, obs1.cmt)
+end
 
 # Simple one-compartment model (uses static vector)
 model = @model begin
-    @param   θ ∈ VectorDomain(3, lower=zeros(3), init=ones(3))
-    @random  η ~ MvNormal(Matrix{Float64}(I, 2, 2))
+    @param begin
+        θ ∈ VectorDomain(4, lower=zeros(4), init=ones(4))
+        Ω ∈ PSDDomain(2)
+        σ ∈ RealDomain(lower=0.0, init=1.0)
+    end
+
+    @random begin
+        η ~ MvNormal(Ω)
+    end
+
+    @covariates sex wt etn
 
     @pre begin
         Ka = θ[1]
-        CL = θ[2]*exp(η[1])
-        V  = θ[3]*exp(η[2])
+        CL = θ[2] * ((wt/70)^0.75) * (θ[4]^sex) * exp(η[1])
+        V  = θ[3] * exp(η[2])
     end
 
     @dynamics begin
+        cp       =  Central/V
         Depot'   = -Ka*Depot
-        Central' =  Ka*Depot - (CL/V)*Central
+        Central' =  Ka*Depot - CL*cp
     end
 
     @derived begin
-        cp = @. Central / V
-        cmax = maximum(cp)
-        pp = exp(cmax) / (1 + exp(cmax)) # pain probability
+        conc = @. Central / V
+        cmax = maximum(conc)
+        dv ~ @. Normal(conc, conc*σ)
     end
 end
 
 # Function-Based Interface (non-static vector)
-p = ParamSet((θ = VectorDomain(3, lower=zeros(3), init=ones(3)),))
-rfx_f(p) = RandomEffectSet((η=MvNormal(Matrix{Float64}(I, 2, 2)),))
+p = ParamSet((θ = VectorDomain(3, lower=zeros(3), init=ones(3)),
+              Ω = PSDDomain(2),
+              σ = RealDomain(lower=0.0, init=1.0)))
+rfx_f(p) = RandomEffectSet((η=MvNormal(p.Ω),))
 function col_f(p,rfx,cov)
     (Ka = p.θ[1],
-    CL = p.θ[2] * exp(rfx.η[1]),
-    V  = p.θ[3] * exp(rfx.η[2]))
+     CL = p.θ[2] * ((cov.wt/70)^0.75) * (p.θ[4]^cov.sex) * exp(rfx.η[1]),
+     V  = p.θ[3] * exp(rfx.η[2]),
+     σ = p.σ)
 end
 init_f(col,t0) = [0.0,0.0]
 function onecompartment_f(du,u,p,t)
-    du[1] = -p.Ka*u[1]
-    du[2] = p.Ka*u[1] - (p.CL/p.V)*u[2]
+    Depot, Central = u # TODO: use field access after LabelledArray fix
+    cp = Central/p.V
+    du[1] = -p.Ka*Depot
+    du[2] = p.Ka*Depot - p.CL*cp
 end
 prob = ODEProblem(onecompartment_f,nothing,nothing,nothing)
 function derived_f(col,sol,obstimes)
-    central = map(x->x[2], sol)
-    cp = @. central / col.V
-    cmax = maximum(cp)
-    pp = exp(cmax) / (1 + exp(cmax))
-    (cmax = cmax, pp = pp), ()
+    central = map(x->x[2], sol) # TODO: use field access after LabelledArray fix
+    _conc = @. central / col.V
+    _cmax = maximum(_conc)
+    _dv = @. Normal(_conc, _conc*col.σ)
+    (conc = _conc, cmax = _cmax), (dv = _dv,)
 end
 model_ip = PKPDModel(p,rfx_f,col_f,init_f,prob,derived_f)
 
+# Initial data
+θ0 = [2.268,74.17,468.6,0.5876]
+x0 = (θ = θ0, Ω = PDMat([0.05 0.0; 0.0 0.2]), σ = 0.1)
+Random.seed!(0); y0 = init_random(model, x0)
+
 # Test gradient and hessian of an observable w.r.t. θ
-function test_fun(model, observable)
+function test_obs(model)
     function (θ)
-        x0 = (θ = θ,)
-        y0 = (η = eltype(θ).([0.0,0.0]),)
-        sim = simobs(model, subject, x0, y0; abstol=1e-14, reltol=1e-14)
-        sim[observable]
+        _x0 = (θ = θ, Ω = x0.Ω, σ = x0.σ)
+        _y0 = (η = eltype(θ).(y0.η),)
+        sim = simobs(model, subject, _x0, _y0; abstol=1e-14, reltol=1e-14)
+        sim[:cmax]
     end
 end
 
-θ0 = [1.5, 1.0, 30.0]
 # Static vectors currently breaks due to type conversion error
-fun = test_fun(model, :cmax)
-@test_broken FD_gradient(fun, θ0) ≈ AD_gradient(fun, θ0) atol=1e-10
-@test_broken FD_hessian(fun, θ0) ≈ AD_hessian(fun, θ0) atol=1e-4
-fun = test_fun(model, :pp)
-@test_broken FD_gradient(fun, θ0) ≈ AD_gradient(fun, θ0) atol=1e-10
-@test_broken FD_hessian(fun, θ0) ≈ AD_hessian(fun, θ0) atol=1e-4
+fun = test_obs(model)
+@test_broken FD_gradient(fun, θ0) ≈ AD_gradient(fun, θ0) atol=1e-8
+@test_broken FD_hessian(fun, θ0) ≈ AD_hessian(fun, θ0) atol=1e-3
 
-fun = test_fun(model_ip, :cmax)
-@test FD_gradient(fun, θ0) ≈ AD_gradient(fun, θ0) atol=1e-10
-@test FD_hessian(fun, θ0) ≈ AD_hessian(fun, θ0) atol=1e-4
-fun = test_fun(model_ip, :pp)
-@test FD_gradient(fun, θ0) ≈ AD_gradient(fun, θ0) atol=1e-10
-@test FD_hessian(fun, θ0) ≈ AD_hessian(fun, θ0) atol=1e-4
+fun = test_obs(model_ip)
+@test FD_gradient(fun, θ0) ≈ AD_gradient(fun, θ0) atol=1e-8
+@test FD_hessian(fun, θ0) ≈ AD_hessian(fun, θ0) atol=1e-3
+
+# Test gradient and hessian of the total log likelihood w.r.t. θ
+function test_likelihood(model)
+    function (θ)
+        _x0 = (θ = θ, Ω = x0.Ω, σ = x0.σ)
+        _y0 = (η = eltype(θ).(y0.η),)
+        likelihood(model, subject, _x0, _y0; abstol=1e-14, reltol=1e-14)
+    end
+end
+
+fun = test_likelihood(model)
+@test_broken FD_gradient(fun, θ0) ≈ AD_gradient(fun, θ0) atol=1e-8
+@test_broken FD_hessian(fun, θ0) ≈ AD_hessian(fun, θ0) atol=1e-3
+
+fun = test_likelihood(model_ip)
+# TODO: fix likelihood problem first
+# @test FD_gradient(fun, θ0) ≈ AD_gradient(fun, θ0) atol=1e-8
+# @test FD_hessian(fun, θ0) ≈ AD_hessian(fun, θ0) atol=1e-3
+@show maximum(abs.(FD_gradient(fun, θ0) - AD_gradient(fun, θ0)))
+@show maximum(abs.(FD_hessian(fun, θ0) - AD_hessian(fun, θ0)))
