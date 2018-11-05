de305ced37ac6548c5731dd1cc35e201ece9ce4a
Xingjian Guo <xg703@nyu.edu>
2018-11-05 10:30:05 -0500

    Restructure `adjust_event_timings`

--- src/simulate_methods/utils.jl
+++ src/simulate_methods/utils.jl
@@ -1,43 +1,42 @@
-function adjust_event_timings(events,args...)
-  adjusted_events = copy(events) # shallow copy of the collection
-  adjust_event_timings!(adjusted_events,args...)
-  adjusted_events
+_cmt_value(ev::Event, var::Number) = var
+_cmt_value(ev::Event, var) = var[ev.cmt]
+function adjust_event_timings(events::AbstractVector{Event},args...)
+  out = collect(adjust_event_timings(ev, args...) for ev in events)
+  sort!(out)
 end
-function adjust_event_timings!(events,lags,bioav,rate_input,duration_input)
-  for i in eachindex(events)
-    ev = events[i]
-    typeof(rate_input) <: Number ? rate = rate_input : rate = rate_input[ev.cmt]
-    typeof(duration_input) <: Number ? duration = duration_input : duration = duration_input[ev.cmt]
-    @assert rate == 0 || duration == 0
+function adjust_event_timings(ev::Event,lags,bioav,rate_input,duration_input)
+  rate = _cmt_value(ev, rate_input)
+  duration = _cmt_value(ev, duration_input)
+  @assert rate == 0 || duration == 0
 
-    if ev.amt != 0
-      if rate != 0
-        _rate = rate
-        _duration = ev.amt
-        typeof(rate) <: Number ? (_duration /= rate) : (_duration /= rate[ev.cmt])
-        typeof(bioav) <: Number ? (_duration *= bioav) : (_duration *= bioav[ev.cmt])
-      elseif duration != 0
-        typeof(duration) <: Number ? (_duration = duration) : (_duration = duration[ev.cmt])
-        _rate = ev.amt/_duration
-        typeof(bioav) <: Number ? (_rate *= bioav) : (_rate *= bioav[ev.cmt])
-      else # both are zero
-        _rate = ev.rate
-        _duration = ev.amt/ev.rate
-        typeof(bioav) <: Number ? (_duration *= bioav) : (_duration *= bioav[ev.cmt])
-      end
-
-      if ev.rate_dir == -1
-        time = ev.base_time + _duration
-      else
-        time = ev.base_time
-      end
-
-      typeof(lags) <: Number ? (time+=lags) : (time+=lags[ev.cmt])
-      events[i] = Event(ev.amt,time,ev.evid, ev.cmt, _rate, _duration,
-                        ev.ss, ev.ii, ev.base_time, ev.rate_dir)
+  if ev.amt != 0
+    if rate != 0
+      _rate = rate
+      _duration = ev.amt
+      _duration /= _cmt_value(ev, rate)
+      _duration *= _cmt_value(ev, bioav)
+    elseif duration != 0
+      _duration = _cmt_value(ev, duration)
+      _rate = ev.amt/_duration
+      _rate *= _cmt_value(ev, bioav)
+    else # both are zero
+      _rate = ev.rate
+      _duration = ev.amt/ev.rate
+      _duration *= _cmt_value(ev, bioav)
     end
+
+    if ev.rate_dir == -1
+      time = ev.base_time + _duration
+    else
+      time = ev.base_time
+    end
+
+    time += _cmt_value(ev, lags)
+    Event(ev.amt, time, ev.evid, ev.cmt, _rate, _duration, ev.ss, ev.ii,
+          ev.base_time, ev.rate_dir)
+  else
+    ev # deep copy instead?
   end
-  sort!(events)
 end
 
 function sorted_approx_unique(event)
