43e05e304bfc27a71969b4ef7a665b6708b3b893
Xingjian Guo <xg703@nyu.edu>
2018-11-17 20:47:09 -0500

    Add magic argument test

--- test/measurement_tests.jl
+++ test/measurement_tests.jl
@@ -117,3 +117,64 @@ end
     err = sim_ms[:cmax].err
     @test err ≈ err_manual atol=1e-2
 end
+
+@testset "Magic argument" begin
+    # Model with magic argument bioav
+    model = @model begin
+        @param begin
+            θ ∈ VectorDomain(4, lower=zeros(4), init=ones(4))
+            Ω ∈ PSDDomain(2)
+            σ ∈ RealDomain(lower=0.0, init=1.0)
+        end
+
+        @random begin
+            η ~ MvNormal(Ω)
+        end
+
+        @covariates sex wt etn
+
+        @pre begin
+            Ka = θ[1]
+            CL = θ[2] * ((wt/70)^0.75) * (θ[4]^sex) * exp(η[1])
+            V  = θ[3] * exp(η[2])
+            bioav = θ[5]
+        end
+
+        @dynamics begin
+            cp       =  Central/V
+            Depot'   = -Ka*Depot
+            Central' =  Ka*Depot - CL*cp
+        end
+
+        @derived begin
+            conc = @. Central / V
+            cmax = maximum(conc)
+        end
+    end
+
+    # Initial data
+    θ0 = [2.268, 74.17, 468.6, 0.5876, 0.412]
+    x0 = (θ = θ0, Ω = PDMat([0.05 0.0; 0.0 0.2]), σ = 0.1)
+    Random.seed!(0); y0 = init_random(model, x0)
+
+    # Introduce measurement uncertainty to θ[1] (Ka)
+    θ_ms = θ0 .± [0.0, 0.0, 0.0, 0.0, 0.02]
+    x0_ms = (θ=θ_ms, Ω=x0.Ω, σ=x0.σ)
+    sol = solve(model, subject, x0_ms, y0; abstol=1e-14, reltol=1e-14)
+    @test sol.u[1] isa SLArray && eltype(sol.u[1]) == Measurement{Float64} # test type stability
+
+    # Compare with manual tracking of uncertainties
+    θ_plus = θ0 .+ [0.0, 0.0, 0.0, 0.0, 0.02]
+    θ_minus = θ0 .- [0.0, 0.0, 0.0, 0.0, 0.02]
+    x0_plus = (θ=θ_plus, Ω=x0.Ω, σ=x0.σ)
+    x0_minus = (θ=θ_minus, Ω=x0.Ω, σ=x0.σ)
+    sim = simobs(model, subject, x0, y0; abstol=1e-14, reltol=1e-14)
+    sim_plus = simobs(model, subject, x0_plus, y0; abstol=1e-14, reltol=1e-14)
+    sim_minus = simobs(model, subject, x0_minus, y0; abstol=1e-14, reltol=1e-14)
+    ## This is only a crude estimate
+    err_manual = max(abs(sim[:cmax] - sim_plus[:cmax]), abs(sim[:cmax] - sim_minus[:cmax]))
+
+    sim_ms = simobs(model, subject, x0_ms, y0; abstol=1e-14, reltol=1e-14)
+    err = sim_ms[:cmax].err
+    @test err ≈ err_manual atol=1e-2
+end
