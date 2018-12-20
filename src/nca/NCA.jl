6bc43798e9f11a07d30acb60be1f9c6484fcc4a2
Yingbo Ma <mayingbo5@gmail.com>
2018-12-20 13:31:24 -0500

    Make f(::NCAPopulation) return an LArray

--- src/nca/NCA.jl
+++ src/nca/NCA.jl
@@ -3,6 +3,7 @@ module NCA
 using Reexport
 using GLM
 @reexport using DataFrames
+using LabelledArrays: LArray
 using Pkg, Dates, Printf
 import ..PuMaS: Formulation, IV, EV
 
@@ -16,17 +17,21 @@ include("simple.jl")
 export NCASubject, NCAPopulation, NCADose, showunits
 export parse_ncadata
 export auc, aumc, lambdaz, auc_extrap_percent, aumc_extrap_percent,
-       clast, clast, tlast, cmax, tmax, cmin, tmin, thalf, cl, clf, vss, vz,
+       clast, tlast, cmax, tmax, cmin, tmin, thalf, cl, clf, vss, vz,
        bioav, tlag, mrt, mat, tau, cavg, fluctation, accumulationindex,
        swing
 export NCAReport
 
-for f in (:lambdaz, :cmax, :tmax, :cmin, :tmin, :clast, :clast, :tlast, :thalf, :cl, :clf, :vss, :vz,
+for f in (:lambdaz, :cmax, :tmax, :cmin, :tmin, :clast, :tlast, :thalf, :cl, :clf, :vss, :vz,
           :interpextrapconc, :auc, :aumc, :auc_extrap_percent, :aumc_extrap_percent,
           :bioav, :tlag, :mrt, :mat, :tau, :cavg, :fluctation, :accumulationindex,
           :swing)
   @eval $f(conc, time, args...; kwargs...) = $f(NCASubject(conc, time; kwargs...), args...; kwargs...)
-  @eval $f(n::NCAPopulation, args...; kwargs...) = map(n->$f(n, args...; kwargs...), n)
+  @eval function $f(pop::NCAPopulation, args...; kwargs...)
+    labels = ((Symbol(:id, subj.id) for subj in pop)...,)
+    params = map(pop->$f(pop, args...; kwargs...), pop)
+    return LArray{labels}(params)
+  end
 end
 
 end
