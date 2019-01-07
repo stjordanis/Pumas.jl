20a6f4a3d01a6fc5a5e3e926e678a6dc769a6738
Unknown <vaibhavyashdixit@gmail.com>
2019-01-07 20:49:08 +0530

    Unit Handling tests broken

--- test/nlme/simple_model.jl
+++ test/nlme/simple_model.jl
@@ -50,6 +50,6 @@ end
 
 Optim.optimize(full_ll,[0.5],BFGS())
 
-
-ml = sum(i -> PuMaS.marginal_nll_nonmem(mdsl1,data[i],x0,(η=[ηstar[i]],),FOCEI()), 1:10)
+ηstar = [-0.114654,0.0350263,-0.024196,-0.0870518,0.0750881,0.059033,-0.114679,-0.023992,-0.0528146,-0.00185361]
+ml = sum(i -> PuMaS.marginal_nll_nonmem(mdsl1,data[i],x0,(η=[ηstar[i]],),PuMaS.FOCEI()), 1:10)
 @test ml ≈ 56.410938825140313 rtol = 1e-6
