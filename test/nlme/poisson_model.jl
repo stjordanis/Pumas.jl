6e72c5100286a0d814c84af515818bbd78c71932
Simon Byrne <simonbyrne@gmail.com>
2019-01-30 14:53:48 -0800

    use LaplaceI

--- test/nlme/poisson_model.jl
+++ test/nlme/poisson_model.jl
@@ -7,24 +7,6 @@ StatsFuns.RFunctions.poisrand(x::ForwardDiff.Dual) = StatsFuns.RFunctions.poisra
 
 df = process_nmtran(example_nmtran_data("sim_poisson"),[:dose])
 
-struct XPoisson{T} <: DiscreteUnivariateDistribution
-  λ::T
-end
-
-Distributions.rand(d::XPoisson) = 0
-function Distributions.logpdf(d::XPoisson, DV::Real)
-  LAM = d.λ
-  LFAC=DV < 1 ? 0.0 : DV*log(DV)-DV+log(DV*(1+4*DV*(1+2*DV)))/6+log(3.1415)/2
-  (-LAM+DV*log(LAM)-LFAC)
-end
-
-
-θ1 = 9.05E-02
-θ2 = 4.75E-02
-Ω  = 4.74E-02
-
-
-
 
 poisson_model = @model begin
     @param begin
@@ -49,38 +31,13 @@ poisson_model = @model begin
 end
 
 
-poisson_xmodel = @model begin
-    @param begin
-        θ ∈ VectorDomain(2,init=[1.02930E+00,4.51854E-01])
-        Ω ∈ PSDDomain(fill(4.74E-02,1,1))
-    end
-
-    @random begin
-        η ~ MvNormal(Ω)
-    end
-
-    @pre begin
-        baseline= θ[1]*exp(η[1])
-        d50 = θ[2]
-        λ = baseline*(1-dose/(dose+d50))
-    end
-
-    @covariates dose
-
-    @derived begin
-        dv ~ @. XPoisson(λ)
-    end
-end
-
 x0 = init_param(poisson_model)
 y0 = init_random(poisson_model, x0)
 
-@test solve(poisson_model,df,x0,y0) === nothing
+@test solve(poisson_model,df[1],x0,y0) === nothing
 @test simobs(poisson_model,df,x0,y0) != nothing
 
 res = simobs(poisson_model,df,x0,y0)
-
-@test_broken PuMaS.marginal_nll_nonmem(poisson_model,df,x0,y0,Laplace()) ≈ 3809.805993678949562 rtol=1e-6
 
 initial_estimates = [-8.31130E-01,
                      -9.51865E-01,
@@ -105,9 +62,9 @@ initial_estimates = [-8.31130E-01,
 
 @testset "initial" begin
   for (i,est) in enumerate(initial_estimates)
-    @test PuMaS.rfx_estimate(poisson_model, df[i], x0, Laplace()) ≈ [est] atol=1e-5
+    @test PuMaS.rfx_estimate(poisson_model, df[i], x0, PuMaS.LaplaceI()) ≈ [est] atol=1e-5
   end
-  @test 2*PuMaS.marginal_nll(poisson_model,df,x0,Laplace()) ≈ 4015.70427796336 atol=1e-1
+  @test 2*PuMaS.marginal_nll(poisson_model,df,x0,PuMaS.LaplaceI()) ≈ 4015.70427796336 atol=1e-1
 end
 
 
