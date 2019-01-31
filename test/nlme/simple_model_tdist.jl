48b55898b9b404975b86782d7e1c48e6eb28d5bd
Andreas Noack <andreas@noack.dk>
2019-01-31 09:53:20 +0100

    Fix simple_model_tdist and include it in runtests.jl

--- test/nlme/simple_model_tdist.jl
+++ test/nlme/simple_model_tdist.jl
@@ -28,32 +28,27 @@ tdist = @model begin
     @dynamics ImmediateAbsorptionModel
 
     @derived begin
-        dv ~ @. LocationScale(conc, conc*sqrt(Σ)+eps(), TDist(250))
+        dv ~ @. LocationScale(conc, conc*sqrt(Σ)+eps(), TDist(30))
     end
 end
 
 
 x0 = init_param(tdist)
 
-for (i, v) in enumerate([-0.114654,0.0350263,-0.024196,-0.0870518,0.0750881,0.059033,-0.114679,-0.023992,-0.0528146,-0.00185361])
-  @test PuMaS.rfx_estimate(tdist, data[i], x0, Laplace())[1] ≈ v rtol=1e-3
+for (d, v) in zip(data, [-0.110095703125000, 0.035454025268555, -0.024982604980469,
+                         -0.085317642211914, 0.071675025939941, 0.059612709045410,
+                         -0.110117317199707, -0.024778854370117, -0.053085464477539,
+                         -0.002428230285645])
+  @test PuMaS.rfx_estimate(tdist, d, x0, PuMaS.LaplaceI())[1] ≈ v rtol=1e-6
 end
 
-@test_broken PuMaS.marginal_nll_nonmem(tdist,data,x0,[0.0],Laplace()) ≈ 57.19397077905644 rtol=1e-6
-@test_broken PuMaS.marginal_nll_nonmem(tdist,data,x0,(η=[0.0],),Laplace()) ≈ 57.19397077905644 rtol=1e-6
-@test_broken PuMaS.marginal_nll_nonmem(tdist,data,x0,Laplace()) ≈ 56.810343602063618 rtol=1e-6
+@test_broken PuMaS.marginal_nll_nonmem(tdist, data, x0, PuMaS.FOCEI()) ≈ -10000 rtol = 1e-6 # Need to get the right value of the mll
+@test_broken PuMaS.marginal_nll_nonmem(tdist, data, x0, PuMaS.FOCE())
+@test PuMaS.marginal_nll_nonmem(tdist, data, x0, PuMaS.LaplaceI()) ≈ 57.112537604068990 rtol=1e-6
 
 function full_ll(θ)
-  _x0 = (θ=θ,Ω=fill(0.04,1,1),Σ=0.1)
-  PuMaS.marginal_nll_nonmem(tdist,data,_x0,Laplace())
+  _x0 = (θ=θ, Ω=fill(0.04, 1, 1), Σ=0.1)
+  PuMaS.marginal_nll_nonmem(tdist, data,_x0, PuMaS.LaplaceI())
 end
 
-Optim.optimize(full_ll,[0.5],BFGS())
-
-ηstar = [-0.114654,0.0350263,-0.024196,-0.0870518,0.0750881,0.059033,-0.114679,-0.023992,-0.0528146,-0.00185361]
-ml = sum(i -> PuMaS.marginal_nll_nonmem(tdist,data[i],x0,(η=[ηstar[i]],),PuMaS.FOCEI()), 1:10)
-@test_broken ml ≈ 56.410938825140313 rtol = 1e-6
-
-ηstar = [-0.1007, 0.0167, -0.0363, -0.0820, 0.1061, 0.0473, -0.1007, -0.0361, -0.0578, -0.0181]
-ml = sum(i -> PuMaS.marginal_nll_nonmem(tdist,data[i],x0,(η=[ηstar[i]],),PuMaS.FOCE()), 1:10)
-@test_broken ml ≈ 56.476216665029462 rtol = 1e-6
+Optim.optimize(full_ll, [0.5], BFGS())
