245e9149b4ea1bef0a23e59f70880cecf6839b81
Xingjian Guo <xg703@nyu.edu>
2019-04-02 11:11:25 -0400

    Add switch for event list inclusion

--- src/models/simulated_observations.jl
+++ src/models/simulated_observations.jl
@@ -11,29 +11,34 @@ end
 @inline function Base.setindex!(obs::SimulatedObservations, x, I...)
   obs.observed[I...] = x
 end
-function DataFrames.DataFrame(obs::SimulatedObservations)
+function DataFrames.DataFrame(obs::SimulatedObservations;
+  include_events=true)
   nrows = length(obs.times)
-  # Generate the expanded event list
-  events = obs.subject.events
-  amt  = zeros(typeof(events[1].amt),  nrows)
-  evid = zeros(typeof(events[1].evid), nrows)
-  cmt  = zeros(typeof(events[1].cmt),  nrows)
-  rate = zeros(typeof(events[1].rate), nrows)
-  for ev in events
-    ind = searchsortedlast(obs.times, ev.time)
-    if obs.times[ind] == ev.time
-      amt[ind]  = ev.amt
-      evid[ind] = ev.evid
-      cmt[ind]  = ev.cmt
-      rate[ind] = ev.rate
-    else
-      @warn "Non-observing event encountered in siumulated observation"
+  if include_events
+    # Generate the expanded event list
+    events = obs.subject.events
+    amt  = zeros(typeof(events[1].amt),  nrows)
+    evid = zeros(typeof(events[1].evid), nrows)
+    cmt  = zeros(typeof(events[1].cmt),  nrows)
+    rate = zeros(typeof(events[1].rate), nrows)
+    for ev in events
+      ind = searchsortedlast(obs.times, ev.time)
+      if obs.times[ind] == ev.time
+        amt[ind]  = ev.amt
+        evid[ind] = ev.evid
+        cmt[ind]  = ev.cmt
+        rate[ind] = ev.rate
+      else
+        @warn "Non-observing event encountered in siumulated observation"
+      end
     end
+    DataFrame(merge(
+      (time=obs.times,), obs.observed,
+      (amt=amt, evid=evid, cmt=cmt, rate=rate)
+    ))
+  else
+    DataFrame(merge((time=obs.times,), obs.observed))
   end
-  df = DataFrame(merge(
-    (time=obs.times,), obs.observed,
-    (amt=amt, evid=evid, cmt=cmt, rate=rate)
-  ))
 end
 
 @recipe function f(obs::SimulatedObservations)
