1a1506ddbd7a93b211d6c2f01b8249f9a2cf3e19
Christopher Rackauckas <Contact@ChrisRackauckas.com>
2019-04-13 08:42:53 -0400

    update modelingtoolkit to 0.2.0 and support tvcov in dsl

--- test/core/time_varying_covar.jl
+++ test/core/time_varying_covar.jl
@@ -45,7 +45,7 @@ function derived_f(col,sol,obstimes,obs)
     (conc = conc,)
 end
 
-mobj = PuMaSModel(p,rfx_f,col_f,init_f,prob,derived_f,(col,sol,obstimes,samples)->samples)
+mobj = PuMaSModel(p,rfx_f,col_f,init_f,prob,derived_f)
 
 param = (θ = [2.268,74.17,468.6,0.5876],
       Ω = PDMat([0.05 0.0;
@@ -55,42 +55,45 @@ subject1 = data.subjects[1]
 randeffs = init_randeffs(mobj, param)
 
 sol_mobj = solve(mobj,subject1,param,randeffs)
+obs_mobj = simobs(mobj,subject1,param,randeffs)
 
 ## DSL
 
-@test_broken begin
+m_diffeq = @model begin
+  @param begin
+      θ ∈ VectorDomain(4, lower=zeros(4), init=ones(4))
+      Ω ∈ PSDDomain(2)
+      σ ∈ RealDomain(lower=0.0, init=1.0)
+  end
 
-  m_diffeq = @model begin
-      @param begin
-          θ ∈ VectorDomain(4, lower=zeros(4), init=ones(4))
-          Ω ∈ PSDDomain(2)
-          σ ∈ RealDomain(lower=0.0, init=1.0)
-      end
+  @random begin
+      η ~ MvNormal(Ω)
+  end
 
-      @random begin
-          η ~ MvNormal(Ω)
-      end
+  @covariates sex wt etn
 
-      @covariates sex wt etn
+  @pre begin
+      Ka = t -> t*θ[1]
+      CL = θ[2] * ((wt/70)^0.75) * (θ[4]^sex) * exp(η[1])
+      V  = θ[3] * exp(η[2])
+  end
 
-      @pre begin
-          Ka = t -> t*θ[1]
-          CL = θ[2] * ((wt/70)^0.75) * (θ[4]^sex) * exp(η[1])
-          V  = θ[3] * exp(η[2])
-      end
+  @vars begin
+      cp = Central/V
+  end
 
-      @vars begin
-          cp = Central/V
-      end
+  @dynamics begin
+      Depot'   = -Ka(t)*Depot
+      Central' =  Ka(t)*Depot - CL*cp
+  end
 
-      @dynamics begin
-          Depot'   = -Ka(t)*Depot
-          Central' =  Ka(t)*Depot - CL*cp
-      end
-
-      @derived begin
-          conc = @. Central / V
-          dv ~ @. Normal(conc, conc*σ)
-      end
+  @derived begin
+      conc = @. Central / V
+      dv ~ @. Normal(conc, conc*σ)
   end
 end
+
+sol_dsl = solve(m_diffeq,subject1,param,randeffs)
+obs_dsl = simobs(m_diffeq,subject1,param,randeffs)
+
+@test obs_dsl.observed.conc ≈ obs_mobj.observed.conc
