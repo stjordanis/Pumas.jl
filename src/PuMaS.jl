906dd2838cf3d4ae898fd1200c6db0d521ac7ebb
Patrick Kofod Mogensen <patrick.mogensen@gmail.com>
2019-04-30 22:09:31 +0200

    StatsBase is now already imported.

--- src/PuMaS.jl
+++ src/PuMaS.jl
@@ -3,7 +3,7 @@ module PuMaS
 using DiffEqBase, DiffEqMonteCarlo, Reexport, StatsBase,
       StaticArrays, DiffEqJump, Distributed, LabelledArrays, GLM,
       TreeViews, CSV, DelayDiffEq, ForwardDiff, DiffResults, Optim,
-      Missings, RecipesBase, StructArrays, RecursiveArrayTools, StatsBase
+      Missings, RecipesBase, StructArrays, RecursiveArrayTools
 
 @reexport using OrdinaryDiffEq, Unitful
 @reexport using Distributions, PDMats, DataFrames
