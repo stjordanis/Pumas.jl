1d6d040c1c0f72812520b1bcfc6f3d0bb3e1ea45
shubham maddhashiya <shubhamsipah@iitkgp.ac.in>
2019-05-20 15:09:18 +0530

    added arg to choose ode alg

--- src/ivivc/deconvo_methods.jl
+++ src/ivivc/deconvo_methods.jl
@@ -1,11 +1,11 @@
 # shooting method to estimate input_rate function
 # error function to be optimized
-function errfun(C_act, t, r, kel, param)
+function errfun(C_act, t, r, kel, param, alg)
   f(u,p,t) = r(u, t, param) - kel*u
   c0 = zero(eltype(C_act))
   tspan = (t[1], t[end])
   prob = ODEProblem(f, c0, tspan)
-  C = solve(prob, Tsit5(), reltol=1e-8, abstol=1e-8, saveat=t).u
+  C = solve(prob, alg, reltol=1e-8, abstol=1e-8, saveat=t).u
   mse = sum(abs2.(C_act .- C))/length(C)
   mse
 end
@@ -16,14 +16,14 @@ function calc_input_rate(subj::VivoSubject, r, p0)
   calc_input_rate(subj.conc, subj.time, r, kel, p0)
 end
 
-function calc_input_rate(C_act, t, r, kel, p0, alg=LBFGS(), box=false, lb=nothing, ub=nothing)
+function calc_input_rate(C_act, t, r, kel, p0; d_alg=Tsit5(), alg=LBFGS(), box=false, lb=nothing, ub=nothing)
   if box == false
-    mfit = optimize(p->errfun(C_act, t, r, kel, p), p0, alg)
+    mfit = optimize(p->errfun(C_act, t, r, kel, p, d_alg), p0, alg)
   else
     if lb == nothing || ub == nothing
       error("lower or upper bound should not be nothing")
     end
-    od = OnceDifferentiable(p->errfun(C_act, t, r, kel, p), p0, autodiff=:finite)
+    od = OnceDifferentiable(p->errfun(C_act, t, r, kel, p, d_alg), p0, autodiff=:finite)
     mfit = optimize(od, lb, ub, p0, Fminbox(alg))
   end
   pmin = Optim.minimizer(mfit)
