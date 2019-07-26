0afd8d48bc25338f0e31b9b2fbd7c3c31129fdaa
shubham maddhashiya <shubhamsipah@iitkgp.ac.in>
2019-07-26 12:54:48 -0400

    changed percentage_pred_cal function

--- src/ivivc/model_validation.jl
+++ src/ivivc/model_validation.jl
@@ -9,19 +9,19 @@
 """
 
 # Internal Validaton
-function percentage_prediction_error(t, C_act, C_pre)
-  sub_act = NCASubject(C_act, t)
-  sub_pre = NCASubject(C_pre, t)
+function percentage_prediction_error(t_act, C_act, t_pre, C_pre)
+  sub_act = NCASubject(C_act, t_act)
+  sub_pre = NCASubject(C_pre, t_pre)
   # AUC Prediction Error
   auc_act = NCA.auc(sub_act, auctype=:last)
   auc_pre = NCA.auc(sub_pre, auctype=:last)
-  auc_pe = (auc_act - auc_pre)/auc_act * 100.0
+  auc_pe = (auc_pre - auc_act)/auc_act * 100.0
   # Cmax Prediction Error
   cmax_act = NCA.cmax(sub_act)
   cmax_pre = NCA.cmax(sub_pre)
-  cmax_pe = (cmax_act - cmax_pre)/cmax_act * 100.0
+  cmax_pe = (cmax_pre - cmax_act)/cmax_act * 100.0
   # return cmax_pe and auc_pe
-  cmax_pe, auc_pe
+  abs(cmax_pe), abs(auc_pe)
 end
 
 # External Validaton
