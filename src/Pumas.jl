b02d1ada8fc2936301c006b8a9d15591b73640b3
Chris Rackauckas <accounts@chrisrackauckas.com>
2019-08-01 08:15:52 -0400

    top level all usings

--- src/Pumas.jl
+++ src/Pumas.jl
@@ -4,7 +4,7 @@ using DiffEqBase, DiffEqDiffTools, Reexport, StatsBase,
       StaticArrays, DiffEqJump, Distributed, LabelledArrays, GLM,
       TreeViews, CSV, DelayDiffEq, ForwardDiff, DiffResults, Optim,
       Missings, RecipesBase, StructArrays, RecursiveArrayTools, HCubature
-using  AdvancedHMC
+using  AdvancedHMC, MCMCChains
 
 import DataInterpolations
 
