f5f378d6100445ea5315911edb4daf34d0ee2791
shubham maddhashiya <shubhamsipah@iitkgp.ac.in>
2019-08-13 06:58:43 +0530

    haskey to hasproperty

--- src/ivivc/data_parsing.jl
+++ src/ivivc/data_parsing.jl
@@ -3,8 +3,8 @@
     read_vivo(df::Union{DataFrame,AbstractString}; id=:id, time=:time,
              conc=:conc, form=:form, dose=:dose, kwargs...)
 
-Parse a `DataFrame` object or a CSV file to `VIVOSubject` or `VIVOPopulation`
-which holds an array of `VIVOSubject`s.
+Parse a `DataFrame` object or a CSV file to `VivoData`
+which holds an array of `VivoForm`s.
 """
 read_vivo(file::AbstractString; kwargs...) = read_vivo(CSV.read(file); kwargs...)
 
@@ -17,9 +17,9 @@ function read_vivo(df; group=nothing, kwargs...)
   return pop
 end
 
-function ___read_vivo(df; id=:id, time=:time, conc=:conc, form=:form, dose=:dose, kwargs...)
+function ___read_vivo(df; id=:id, time=:time, conc=:conc, formulation=:form, dose=:dose, kwargs...)
   local ids, times, concs, forms, doses
-  key_check = haskey(df, id) && haskey(df, time) && haskey(df, conc) && haskey(df, form) && haskey(df, dose)
+  key_check = hasproperty(df, id) && hasproperty(df, time) && hasproperty(df, conc) && hasproperty(df, form) && hasproperty(df, dose)
   if !key_check
     @info "The CSV file has keys: $(names(df))"
     throw(ArgumentError("The CSV file must have: id, time, conc, form, dose"))
@@ -40,12 +40,12 @@ function ___read_vivo(df; id=:id, time=:time, conc=:conc, form=:form, dose=:dose
   for (i, id) in enumerate(uids)
     # id's range, and we know that it is sorted
     idx = findfirst(isequal(id), ids):findlast(isequal(id), ids)
-    ind = Dict{eltype(forms), VivoSubject}()
+    ind = Dict{eltype(forms), VivoForm}()
     for form in uforms
       if form in forms[idx]
         idx_n = findfirst(isequal(form), forms[idx]):findlast(isequal(form), forms[idx])
         try
-          ind[form] = VivoSubject(concs[idx_n], times[idx_n], form, doses[idx_n[1]], id, kwargs...)
+          ind[form] = VivoForm(concs[idx_n], times[idx_n], form, doses[idx_n[1]], id, kwargs...)
         catch
           @info "ID $id errored for formulation $(form)"
           rethrow()
@@ -55,7 +55,7 @@ function ___read_vivo(df; id=:id, time=:time, conc=:conc, form=:form, dose=:dose
     ncas[i] = ind
   end
   # Use broadcast to tighten ncas element type
-  pop = VivoPopulation(identity.(ncas))
+  pop = VivoData(identity.(ncas))
   return pop
 end
 
@@ -65,8 +65,8 @@ end
     read_vitro(df::Union{DataFrame,AbstractString}; id=:id, time=:time,
              conc=:conc, form=:form, kwargs...)
 
-Parse a `DataFrame` object or a CSV file to `VitroSubject` or `VitroPopulation`
-which holds an array of `VitroSubject`s.
+Parse a `DataFrame` object or a CSV file to `VitroData`
+which holds an array of `VitroForm`s.
 """
 read_vitro(file::AbstractString; kwargs...) = read_vitro(CSV.read(file); kwargs...)
 
@@ -81,7 +81,7 @@ end
 
 function ___read_vitro(df; id=:id, time=:time, conc=:conc, form=:form, kwargs...)
   local ids, times, concs, forms
-  key_check = haskey(df, id) && haskey(df, time) && haskey(df, conc) && haskey(df, form)
+  key_check = hasproperty(df, id) && hasproperty(df, time) && hasproperty(df, conc) && hasproperty(df, form)
   if !key_check
     @info "The CSV file has keys: $(names(df))"
     throw(ArgumentError("The CSV file must have: id, time, conc, form"))
@@ -101,12 +101,12 @@ function ___read_vitro(df; id=:id, time=:time, conc=:conc, form=:form, kwargs...
   for (i, id) in enumerate(uids)
     # id's range, and we know that it is sorted
     idx = findfirst(isequal(id), ids):findlast(isequal(id), ids)
-    ind = Dict{eltype(forms), VitroSubject}()
+    ind = Dict{eltype(forms), VitroForm}()
     for form in uforms
       if form in forms[idx]
         idx_n = findfirst(isequal(form), forms[idx]):findlast(isequal(form), forms[idx])
         try
-          ind[form] = VitroSubject(concs[idx_n], times[idx_n], form, id, kwargs...)
+          ind[form] = VitroForm(concs[idx_n], times[idx_n], form, id, kwargs...)
         catch
           @info "ID $id errored for formulation $(form)"
           rethrow()
@@ -116,6 +116,6 @@ function ___read_vitro(df; id=:id, time=:time, conc=:conc, form=:form, kwargs...
     ncas[i] = ind
   end
   # Use broadcast to tighten ncas element type
-  pop = VitroPopulation(identity.(ncas))
+  pop = VitroData(identity.(ncas))
   return pop
 end
