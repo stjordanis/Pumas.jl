c7b6e4a33fd86cc973fd6591862c98bb2835cf10
Anshul Singhvi <asinghvi17@simons-rock.edu>
2019-08-14 13:06:38 -0400

    Remove LOESS series recipe (#639)
    Superceded by PumasAI/DataInterpolations.jl#37

--- src/plotting/plotting.jl
+++ src/plotting/plotting.jl
@@ -3,26 +3,6 @@
 ################################################################################
 
 ########################################
-#                Loess                 #
-########################################
-
-@recipe function f(::Type{Val{:loess}}, x, y, z)
-    ox, oy = x, y
-
-    loess_resolution = minimum(diff(ox)) / 4 # the minimum diffference in xs, div by 4.  TODO is there a better heuristic?
-
-    model = Loess.loess(ox, oy)
-    loess_fit_xs = range(minimum(ox), maximum(ox), step=loess_resolution)
-    loess_fit_ys = Loess.predict(model, loess_fit_xs)
-
-    label --> "LOESS fit"
-
-    seriestype := :path
-    x := loess_fit_xs
-    y := loess_fit_ys
-end
-
-########################################
 #            Identity line             #
 ########################################
 
