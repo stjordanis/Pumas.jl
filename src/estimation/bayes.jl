6b9ed4452e03f9240287d71d6dd06b4b12d34b57
Andreas Noack <andreas@noack.dk>
2019-08-14 21:39:53 +0200

    Remove try/catch blocks in bayesian likelihood evaluations. They are no longer required.

--- src/estimation/bayes.jl
+++ src/estimation/bayes.jl
@@ -37,81 +37,65 @@ function dimension(b::BayesLogDensity)
 end
 
 function logdensity(b::BayesLogDensity, v::AbstractVector)
-  try
-    m = b.dim_param
-    param = b.model.param
-    t_param = totransform(param)
-    # compute the prior density and log-Jacobian
-    x, j_x = TransformVariables.transform_and_logjac(t_param, @view v[1:m])
-    ℓ_param = _lpdf(param.params, x) + j_x
+  m = b.dim_param
+  param = b.model.param
+  t_param = totransform(param)
+  # compute the prior density and log-Jacobian
+  x, j_x = TransformVariables.transform_and_logjac(t_param, @view v[1:m])
+  ℓ_param = _lpdf(param.params, x) + j_x
 
-    n = b.dim_rfx
-    rfx = b.model.random(x)
-    t_rfx = totransform(rfx)
-    ℓ_rfx = sum(enumerate(b.data)) do (i,subject)
-      # compute the random effect density, likelihood and log-Jacobian
-      y, j_y = TransformVariables.transform_and_logjac(t_rfx, @view v[(m+(i-1)*n) .+ (1:n)])
-      j_y - Pumas.penalized_conditional_nll(b.model, subject, x, y, b.args...; b.kwargs...)
-    end
-    ℓ = ℓ_param + ℓ_rfx
-    return isnan(ℓ) ? -Inf : ℓ
-  catch e
-    if e isa ArgumentError
-      return -Inf
-    else
-      rethrow(e)
-    end
+  n = b.dim_rfx
+  rfx = b.model.random(x)
+  t_rfx = totransform(rfx)
+  ℓ_rfx = sum(enumerate(b.data)) do (i,subject)
+    # compute the random effect density, likelihood and log-Jacobian
+    y, j_y = TransformVariables.transform_and_logjac(t_rfx, @view v[(m+(i-1)*n) .+ (1:n)])
+    j_y - Pumas.penalized_conditional_nll(b.model, subject, x, y, b.args...; b.kwargs...)
   end
+  ℓ = ℓ_param + ℓ_rfx
+  return isnan(ℓ) ? -Inf : ℓ
 end
 
 function logdensitygrad(b::BayesLogDensity, v::AbstractVector)
   ∇ℓ = zeros(size(v))
-  try
-    param = b.model.param
-    t_param = totransform(param)
-    m = b.dim_param
-    n = b.dim_rfx
+  param = b.model.param
+  t_param = totransform(param)
+  m = b.dim_param
+  n = b.dim_rfx
 
-    function L_param(u)
-      x, j_x = TransformVariables.transform_and_logjac(t_param, u)
-      _lpdf(param.params, x) + j_x
-    end
-    fill!(b.buffer, 0.0)
-    copyto!(b.buffer, 1, v, 1, m)
-
-    ForwardDiff.gradient!(b.res, L_param, b.buffer, b.cfg, Val{false}())
-
-    ℓ = DiffResults.value(b.res)
-    ∇ℓ[1:m] .= @view DiffResults.gradient(b.res)[1:m]
-
-    for (i, subject) in enumerate(b.data)
-      # to avoid dimensionality problems with ForwardDiff.jl, we split the computation to
-      # compute the gradient for each subject individually, then accumulate this to the
-      # gradient vector.
-
-      function L_rfx(u)
-        x = TransformVariables.transform(t_param, @view u[1:m])
-        rfx = b.model.random(x)
-        t_rfx = totransform(rfx)
-        y, j_y = TransformVariables.transform_and_logjac(t_rfx, @view u[m .+ (1:n)])
-        j_y - Pumas.penalized_conditional_nll(b.model, subject, x, y, b.args...; b.kwargs...)
-      end
-      copyto!(b.buffer, m+1, v, m+(i-1)*n+1, n)
-
-      ForwardDiff.gradient!(b.res, L_rfx, b.buffer, b.cfg, Val{false}())
-
-      ℓ += DiffResults.value(b.res)
-      ∇ℓ[1:m] .+= @view DiffResults.gradient(b.res)[1:m]
-      copyto!(∇ℓ, m+(i-1)*n+1, DiffResults.gradient(b.res), m+1, n)
-    end
-    return isnan(ℓ) ? -Inf : ℓ, ∇ℓ
-  catch e
-    if e isa ArgumentError
-      return -Inf, ∇ℓ
-    else
-      rethrow(e)
-    end
+  function L_param(u)
+    x, j_x = TransformVariables.transform_and_logjac(t_param, u)
+    _lpdf(param.params, x) + j_x
   end
+  fill!(b.buffer, 0.0)
+  copyto!(b.buffer, 1, v, 1, m)
+
+  ForwardDiff.gradient!(b.res, L_param, b.buffer, b.cfg, Val{false}())
+
+  ℓ = DiffResults.value(b.res)
+  ∇ℓ[1:m] .= @view DiffResults.gradient(b.res)[1:m]
+
+  for (i, subject) in enumerate(b.data)
+    # to avoid dimensionality problems with ForwardDiff.jl, we split the computation to
+    # compute the gradient for each subject individually, then accumulate this to the
+    # gradient vector.
+
+    function L_rfx(u)
+      x = TransformVariables.transform(t_param, @view u[1:m])
+      rfx = b.model.random(x)
+      t_rfx = totransform(rfx)
+      y, j_y = TransformVariables.transform_and_logjac(t_rfx, @view u[m .+ (1:n)])
+      j_y - Pumas.penalized_conditional_nll(b.model, subject, x, y, b.args...; b.kwargs...)
+    end
+    copyto!(b.buffer, m+1, v, m+(i-1)*n+1, n)
+
+    ForwardDiff.gradient!(b.res, L_rfx, b.buffer, b.cfg, Val{false}())
+
+    ℓ += DiffResults.value(b.res)
+    ∇ℓ[1:m] .+= @view DiffResults.gradient(b.res)[1:m]
+    copyto!(∇ℓ, m+(i-1)*n+1, DiffResults.gradient(b.res), m+1, n)
+  end
+  return isnan(ℓ) ? -Inf : ℓ, ∇ℓ
 end
 
 # results wrapper object
