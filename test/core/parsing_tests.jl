56f3ddf10058e02fae648f63dd5e803bf86d0c72
José Bayoán Santiago Calderón <naoyabpr@gmail.com>
2019-08-28 08:17:15 -0400

    Revert "Add test"
    This reverts commit 139e3e86870dbbec3b13b197c92b8934530d0ecf.

--- test/core/parsing_tests.jl
+++ test/core/parsing_tests.jl
@@ -93,10 +93,3 @@ end
   output = read_pumas(data)
   @test ismissing(output[1].observations.dv[1])
 end
-
-@testset "amt = rate * duration" begin
-  e1 = DosageRegimen(100, rate = 25)
-  e2 = DosageRegimen(100, duration = 4)
-  @test e1.data == e2.data
-  @test_throws AssertionError DosageRegimen(100, duration = 4, rate = 20)
-end
